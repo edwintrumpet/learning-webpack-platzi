a26cb2f3f9613a2a5f176ec46bf9dbc84c4c2f0f
Edwin García <spark.com.co@gmail.com>
2020-04-29 17:54:51 -0500

    Modern javaScript support with Webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,6 +14,12 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+                
+            },
+            {
                 test: /\.css$/,
                 use: [
                     // Inject the CSS using javaScript
