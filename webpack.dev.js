05871c7038c87e94f606db88fed16ff34fac50dc
Edwin García <spark.com.co@gmail.com>
2020-04-29 09:46:17 -0500

    Many webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     mode: 'development'
