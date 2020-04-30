c1dfaa43493804ef4b3c6d567e9e5c7655965474
Edwin García <spark.com.co@gmail.com>
2020-04-29 19:09:06 -0500

    Something else with React

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from '../../data.json'
+import Loader from './Loader'
+
+console.log(data)
 
 export default function App() {
+    const [loaderList, setLoaderList] = useState([])
+
+    function handleClick() {
+        setLoaderList(data.loaders)
+    }
     return (
-        <h1>React works!</h1>
+        <div>
+            <ul>
+                {
+                    loaderList.map(element => <Loader key={element.id} name={element.name} />)
+                }
+            </ul>
+            <button onClick={handleClick}>Show what you have learned so far</button>
+        </div>
     )
 }
